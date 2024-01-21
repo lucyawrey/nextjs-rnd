86380a317a31e44b31c6d843409dd1fb9857f355
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-21 00:02:51 -0500

    moved trpc provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { TrpcProvider } from "app/TrpcProvider";
+import { TrpcProvider } from "components/TrpcProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
